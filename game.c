34fdb49fa23dd031b50d42559300a61193cd3a9a
Jonathon Oram Howe <joh29@cs19112bs.canterbury.ac.nz>
2019-10-12 21:27:35 +1300

    Added some comments

--- game.c
+++ game.c
@@ -3,7 +3,7 @@
 #include "navswitch.h"
 
 
-/** Define PIO pins driving LED matrix rows.  */
+/* Define PIO pins driving LED matrix rows.  */
 static const pio_t rows[] =
 {
     LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO,
@@ -12,7 +12,7 @@ static const pio_t rows[] =
 };
 
 
-/** Define PIO pins driving LED matrix columns.  */
+/* Define PIO pins driving LED matrix columns.  */
 static const pio_t cols[] =
 {
     LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
@@ -22,6 +22,10 @@ static const pio_t cols[] =
 
 static void display_column (uint8_t row_pattern, uint8_t current_column)
 {
+    /* Set the pattern on the LED matrix in column current_column (int
+     * in range 0 to 4 (inclusive)) to row_pattern (integer that in
+     * binary represents the LED pattern. */
+
     static int last_col = 0;
     pio_output_high(cols[last_col]);
 
@@ -48,6 +52,7 @@ static void display_column (uint8_t row_pattern, uint8_t current_column)
 
 void initialise_display(void)
 {
+    /* Initialise all rows and columns of the LED matrix display */
     for (int col = 0; col < 5; col ++) {
         pio_config_set(cols[col], PIO_OUTPUT_HIGH);
     }
@@ -59,6 +64,10 @@ void initialise_display(void)
 
 int move_player(int current_x)
 {
+    /* Given the current x coordinate of the player, increase or
+     * decrese it if the navswitch is pushed north or south repectively
+     * if it will stay on screen (in range 0 to 6 (inclusive) */
+
     if (navswitch_push_event_p (NAVSWITCH_SOUTH) && current_x < 6) {
         current_x += 1;
     } else if (navswitch_push_event_p (NAVSWITCH_NORTH) && current_x > 0) {

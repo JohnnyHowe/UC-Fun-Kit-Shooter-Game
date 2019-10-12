bb0a1ff6426336b45014cc828c77cd2bca7470ec
Jonathon Oram Howe <joh29@cs19112bs.canterbury.ac.nz>
2019-10-12 23:29:07 +1300

    Forgot the show_player definition whoops

--- player.h
+++ player.h
@@ -5,8 +5,10 @@
 
 typedef struct {
     /** Player struct, just holds the position right now */
-    int pos;    // Just the x coordinate, x is just a bad name
+    int xPos;
 } Player;
 
 
 void move_player(Player* player);
+
+void show_player(Player* player);

51f9db769d6c229d7b43b04d84450631791db0c7
Jonathon Oram Howe <joh29@cs19117bs.canterbury.ac.nz>
2019-10-15 10:48:29 +1300

    Nice shot.c docstrings

--- shot.c
+++ shot.c
@@ -13,10 +13,10 @@
 void transmit_shot(Shot* shots, int num_shots, int player_number)
 {
     /** Given the array of shots, check to see if any are ready to be
-     * transmitted to the other kit (if x_pos = -1) and if so, transmit.
-     * Parameters: shots, num_shots.
-     * Outputs: IR communication to other device
-     * */
+     * transmitted to the other kit and if so, transmit.
+     * shots (Shot*): shot array to check and possibly transmit from.
+     * num_shots (int): length of shots array.
+     * player_number (int): Player/unit number to transmit from. */
     int x_pos = pos_to_transmit(shots, num_shots);
     if (x_pos != -1) {
         uint8_t message = x_pos;
@@ -28,7 +28,8 @@ void transmit_shot(Shot* shots, int num_shots, int player_number)
 
 void show_shot(Shot* shot)
 {
-    /** Given a Shot pointer, show it if it is on the screen. */
+    /** Show a single shot on the LED matrix..
+     * shot (Shot*): pointer to shot to show. */
     if (shot->y_pos >= 0 && shot->y_pos <= 6) {
         display_column(1 << shot->y_pos, shot->x_pos);
     }
@@ -37,8 +38,9 @@ void show_shot(Shot* shot)
 
 void show_shots(Shot* shots, int num_shots)
 {
-    /** Given a (pointer to) an array of shots and the number of shots
-     * in said array, show them all (run show_shot on them) */
+    /** Show all the shots in the given shot array.
+     * shots (Shot*): Array of shots to show.
+     * num_shots (int): length of shots. */
     for (int i = 0; i < num_shots; i++) {
         show_shot(&shots[i]);
     }
@@ -47,14 +49,17 @@ void show_shots(Shot* shots, int num_shots)
 
 void move_shot(Shot* shot)
 {
-    /** Move the given shot (pointer) forward one unit. */
+    /** Move the given shot forward one unit.
+     * shot (Shot*): shot to move. */
     shot->y_pos += shot->direction;
 };
 
 
 void update_shots(Shot* shots, int num_shots)
 {
-    /** Update the array of shots given (move) */
+    /** Update the array of shots given (move)
+     * shots (Shot*): array of shots to update.
+     * num_shots (int): length of shots array. */
     for (int i = 0; i < num_shots; i++) {
         move_shot(&(shots[i]));
     }
@@ -63,8 +68,10 @@ void update_shots(Shot* shots, int num_shots)
 
 int pos_to_transmit(Shot* shots, int num_shots)
 {
-    /** Given an array of shots, if one is at y = 0 and it is outgoing,
-     * return its x position, if no shots exist, return -1 */
+    /** Return the x position to transmit. If there is not shot that
+     * should be transmitted, -1 is returned.
+     * shots (Shot*): Array of shots to potentially transmit from.
+     * num_shots (int): length of shots array. */
     int x_pos = -1;
     int i = 0;
     while (i < num_shots && x_pos == -1)
@@ -80,9 +87,11 @@ int pos_to_transmit(Shot* shots, int num_shots)
 
 Shot process_shot(int player_number, uint8_t message)
 {
-    /* If a value between 0 and 6 (inclusive) is received by the IR
-     * sensor, use it as the x coordinate for a new shot, and return
-     * it. If not, return a shot with -1 x position. */
+    /** Return a shot with the x position received from message.
+     * If the x position is not in the range 0 to 4 (inclusive), set the
+     * shots x pos to -1.
+     * player_number (int): Receiving player/unit number.
+     * message (uint8_t): Incoming binary message. */
     Shot shot = {-1, 6, -1};
     int incoming_player_num = check_bit(message, 7);
     message &= ~(1 << 7);
@@ -97,8 +106,8 @@ Shot process_shot(int player_number, uint8_t message)
 
 void set_null_shot(Shot* shot)
 {
-    /** Set the direction to zero and the pos to (-1, -1) */
-    shot->x_pos = -1;
+    /** Set the direction of the shot to 0 and the x position to -1
+     * shot (Shot*): Shot to forget about. */
     shot->x_pos = -1;
     shot->direction = 0;
 }
